378fde455955f998019f32a98dbfccdfd84eb0ad
Gabrielly Ferreira <155073067+gabs-fe@users.noreply.github.com>
2025-04-06 21:56:44 -0300

    Create AT_9.java

---
+++ AT_9.java
@@ -0,0 +1,11 @@
+public class AT_9 {
+    public static void main(String[] args) {
+        for (int numeros = 0; numeros <= 50; numeros++)
+        {
+            if (numeros %2 != 0){
+                System.out.println(numeros);
+            }
+        }
+    }
+}
+
