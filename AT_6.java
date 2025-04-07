85189832726d8248e5429ed61775f6d8caed0a50
Gabrielly Ferreira <155073067+gabs-fe@users.noreply.github.com>
2025-04-06 21:55:36 -0300

    Create AT_6.java

---
+++ AT_6.java
@@ -0,0 +1,9 @@
+public class AT_6
+ {
+    public static void main(String[] args) {
+        for (int numero = 1; numero <= 20; numero++) {
+            System.out.print(" ");
+            System.out.print(numero);
+        }
+    }
+}
